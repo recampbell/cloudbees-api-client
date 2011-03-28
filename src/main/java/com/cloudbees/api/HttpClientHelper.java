b0a0c32eb1933c9b255e9950fc1b51bbd2475fd7
fdonze <fonze@gmail.com>
2011-03-28 12:12:28 -0400

    Move to root module. Rename net.stax package to com.cloudbees; StaxClient to BeesClient. Add Apache 2.0 licensing information.

---
+++ src/main/java/com/cloudbees/api/HttpClientHelper.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2010-2011, CloudBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cloudbees.api;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+
+public class HttpClientHelper {
+    public static HttpClient createClient()
+    {
+        HttpClient client = new HttpClient();
+        String proxyHost = System.getProperty("http.proxyHost");
+        if(proxyHost != null)
+        {
+            int proxyPort = 80;
+            String proxyPortStr = System.getProperty("http.proxyPort");
+            if(proxyPortStr != null)
+            {
+                proxyPort = Integer.parseInt(System.getProperty("http.proxyPort"));
+            }
+            
+            client.getHostConfiguration().setProxy(proxyHost,proxyPort);
+            
+            //if there are proxy credentials available, set those too
+            Credentials proxyCredentials = null;
+            String proxyUser = System.getProperty("http.proxyUser");
+            String proxyPassword = System.getProperty("http.proxyPassword");
+            if(proxyUser != null || proxyPassword != null)
+                proxyCredentials = new UsernamePasswordCredentials(proxyUser, proxyPassword);
+            if(proxyCredentials != null)
+                client.getState().setProxyCredentials(AuthScope.ANY, proxyCredentials);
+        }
+
+        return client;
+    }
+}
